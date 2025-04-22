558aaf9d5812b8e14e6b0cbc2aff76726b9e7f59
MoonFlowww <161968556+MoonFlowww@users.noreply.github.com>
2025-04-22 14:29:06 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,9 +7,9 @@ int main() {
 	Thot::Network model1;
 	model1.add(Thot::Transformer::Titan(Thot::Module::MoE, Thot::Normalization::DyT, Thot::Attention::MLA, Thot::Normalization::RMSE);
 	
-	model1.add(Thot::Layer::RBM(768, 256, Thot::Activation::LeakyReLU, Thot::Initialization::LeCun));
+	model1.add(Thot::Layer::RBM(768, 256, Thot::Activation::LeakyReLU, Thot::Initialization::Xavier));
 	model1.add(256, 256, Thot::Attention::MLA.layer(1))
-	model1.add(Thot::Layer::RBM(256, 64, Thot::Activation::LeakyReLU, Thot::Initialization::LeCun));
+	model1.add(Thot::Layer::RBM(256, 64, Thot::Activation::LeakyReLU, Thot::Initialization::Uniform));
 	model1.add(256, 256, Thot::Attention::MLA.layer(1))
 	model1.add(Thot::Layer::RBM(64, 16, Thot::Activation::LeakyReLU, Thot::Initialization::LeCun));
 	
