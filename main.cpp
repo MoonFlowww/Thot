12d70d6fe52a908cf3e87a92cf5effb88d700aaa
MoonFlowww <161968556+MoonFlowww@users.noreply.github.com>
2025-04-24 13:15:47 +0200

    update

--- main.cpp
+++ main.cpp
@@ -12,36 +12,28 @@ void print_vector(const std::vector<float>& vec) {
 }
 
 int main() {
-	Thot::Network model("Thot Network for XOR testing");
+	Thot::Network model("Thot Network [Squared]");
 
-	model.add(Thot::Layer::FC(2, 6, Thot::Activation::ReLU, Thot::Initialization::He));
-	model.add(Thot::Layer::FC(6, 6, Thot::Activation::ReLU, Thot::Initialization::Xavier));
-	model.add(Thot::Layer::FC(6, 1, Thot::Activation::ReLU, Thot::Initialization::LeCun));
+	model.add(Thot::Layer::FC(1, 16, Thot::Activation::Sigmoid, Thot::Initialization::He));
+	model.add(Thot::Layer::FC(16, 16, Thot::Activation::ReLU, Thot::Initialization::He));
+	model.add(Thot::Layer::FC(16, 16, Thot::Activation::ReLU, Thot::Initialization::He));
+	model.add(Thot::Layer::FC(16, 8, Thot::Activation::ReLU, Thot::Initialization::He));
+	model.add(Thot::Layer::FC(8, 1, Thot::Activation::Linear, Thot::Initialization::He));
 
 
 
-	model.set_optimizer(Thot::Optimizer::SGDM(0.01f));
+	model.set_optimizer(Thot::Optimizer::Adam(0.001f));
+
 	model.summary();
 
-	std::vector<std::vector<float>> x_train = {
-		{0, 0},
-		{0, 1},
-		{1, 0},
-		{1, 1}
-	};
+	std::vector<std::vector<float>> x_train = { {0.1}, {0.2}, {0.3}, {0.4}, {0.5}, {0.6}, {0.7}, {0.8}, {0.9} };
 
-	std::vector<std::vector<float>> y_train = {
-		{0},
-		{1},
-		{1},
-		{0}
-	};
+	std::vector<std::vector<float>> y_train = { {0.01}, {0.04}, {0.09}, {0.16}, {0.25}, {0.36}, {0.49}, {0.64}, {0.81} };
 
-	int epochs = 5000;
 
 	std::cout << "\nTraining XOR function...\n" << std::endl;
 
-	model.train(x_train, y_train, 1000, 1, 0.01f, 50);
+	model.train(x_train, y_train, 1000, 5, 0.01f, 50);
 
 	std::cout << "\nTesting XOR function:\n" << std::endl;
 
