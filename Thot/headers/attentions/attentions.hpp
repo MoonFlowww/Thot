484bc8860c872e6992eb322623c8251436d77be2
moonfloww <hugoifiorillo@gmail.com>
2025-09-03 15:00:55 +0200

    att

--- Thot/headers/attentions/attentions.hpp
+++ Thot/headers/attentions/attentions.hpp
@@ -1,38 +1,31 @@
 #pragma once
 
 #include "../tensor.hpp"
+#include "../activations/activations.hpp"
 #include "../initializations/initializations.hpp"
 #include "../optimizations/optimizations.hpp"
+#include "../layers/layers.hpp"
+
 
 namespace Thot {
 
     class Optimizer;
     class MHAAtt;
 
-    class Attention {
-    protected:
-        std::string name_;
-        bool IsTraining;
-        std::shared_ptr<Optimizer> optimizer_;
-        Utils::Tensor input_cache_;
+    class Attention : public Layer {
+
+
     public:
-        Attention(const std::string& name = "attention") : name_(name), IsTraining(true) {}
-        virtual ~Attention() = default;
+        Attention(const std::string& name = "attention") : Layer(name) {}
+        ~Attention() override = default;
 
         virtual Utils::Tensor forward(const Utils::Tensor& input) = 0;
         virtual Utils::Tensor backward(const Utils::Tensor& gradient_output) = 0;
 
-        std::string get_name() const { return name_; }
         virtual size_t get_flops(int batch_size = 1) const = 0;
         virtual Initialization get_initialization() const { return static_cast<const Attention*>(this)->get_initialization(); }
 
-        void set_training(bool training) { IsTraining = training; }
-        bool is_training() const { return IsTraining; }
-
-        void set_optimizer(std::shared_ptr<Optimizer> optimizer) { optimizer_ = optimizer; }
-
-        virtual int get_input_size() const { return -1; }
-        virtual int get_output_size() const { return -1; }
+        Activation get_activation() const override { return Activation::Linear; }
 
         static std::shared_ptr<Attention> MHA(int embed_dim, int num_heads, Initialization weight_init = Initialization::Xavier, const std::string& name = "MHA");
     };
