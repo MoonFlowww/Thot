15119ee15265e523a9aeedb4134ed9ad0231f6ef
moonfloww <hugoifiorillo@gmail.com>
2025-10-22 14:45:27 +0200

    up

---
+++ src/initialization/apply.hpp
@@ -0,0 +1,65 @@
+#ifndef THOT_APPLY_HPP
+#define THOT_APPLY_HPP
+#include <torch/torch.h>
+
+#include "initialization.hpp"
+
+namespace Thot::Initialization::Details {
+    namespace detail {
+        template <class Module>
+        inline void zero_bias_if_present(const Module& module) {
+            if constexpr (requires { module->bias; }) {
+                if (module->bias.defined()) {
+                    torch::nn::init::zeros_(module->bias);
+                }
+            }
+        }
+    }  // namespace detail
+
+    template <class Module, class Descriptor>
+    inline void apply_module_initialization(const Module& module, const Descriptor& descriptor) {
+        const auto type = descriptor.initialization.type;
+
+        switch (type) {
+            case ::Thot::Initialization::Type::XavierNormal:
+                torch::nn::init::xavier_normal_(module->weight);
+                detail::zero_bias_if_present(module);
+                break;
+            case ::Thot::Initialization::Type::XavierUniform:
+                torch::nn::init::xavier_uniform_(module->weight);
+                detail::zero_bias_if_present(module);
+                break;
+            case ::Thot::Initialization::Type::KaimingNormal:
+                torch::nn::init::kaiming_normal_(module->weight,
+                                                 /*a=*/0.0,
+                                                 torch::nn::init::FanModeType::FanIn,
+                                                 torch::nn::init::NonlinearityType::ReLU);
+                detail::zero_bias_if_present(module);
+                break;
+            case ::Thot::Initialization::Type::KaimingUniform:
+                torch::nn::init::kaiming_uniform_(module->weight,
+                                                  /*a=*/0.0,
+                                                  torch::nn::init::FanModeType::FanIn,
+                                                  torch::nn::init::NonlinearityType::ReLU);
+                detail::zero_bias_if_present(module);
+                break;
+            case ::Thot::Initialization::Type::Dirac:
+                if (module->weight.dim() >= 3) {
+                    torch::nn::init::dirac_(module->weight);
+                }
+                detail::zero_bias_if_present(module);
+                break;
+            case ::Thot::Initialization::Type::Lyapunov:
+                torch::nn::init::orthogonal_(module->weight);
+                detail::zero_bias_if_present(module);
+                break;
+            case ::Thot::Initialization::Type::ZeroBias:
+                detail::zero_bias_if_present(module);
+                break;
+            case ::Thot::Initialization::Type::Default:
+            default:
+                break;
+        }
+    }
+}
+#endif //THOT_APPLY_HPP
--- src/initialization/initialization.hpp
+++ src/initialization/initialization.hpp
@@ -5,7 +5,12 @@
 namespace Thot::Initialization {
     enum class Type {
         Default,
-        XavierNormal,
+        XavierUniform,
+        KaimingNormal,
+        KaimingUniform,
+        ZeroBias,
+        Dirac,
+        Lyapunov,
     };
 
     struct Descriptor {
@@ -14,6 +19,12 @@ namespace Thot::Initialization {
 
     inline constexpr Descriptor Default{Type::Default};
     inline constexpr Descriptor XavierNormal{Type::XavierNormal};
+    inline constexpr Descriptor XavierUniform{Type::XavierUniform};
+    inline constexpr Descriptor KaimingNormal{Type::KaimingNormal};
+    inline constexpr Descriptor KaimingUniform{Type::KaimingUniform};
+    inline constexpr Descriptor ZeroBias{Type::ZeroBias};
+    inline constexpr Descriptor Dirac{Type::Dirac};
+    inline constexpr Descriptor Lyapunov{Type::Lyapunov};
 }
 
 #endif //THOT_INITIALIZATION_HPP
