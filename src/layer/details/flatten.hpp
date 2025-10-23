27f878360bc7d4d8452bf6be06b9d21b2c6810a9
moonfloww <hugoifiorillo@gmail.com>
2025-10-23 15:53:31 +0200

    up

--- src/layer/details/flatten.hpp
+++ src/layer/details/flatten.hpp
@@ -1,7 +1,10 @@
 #ifndef THOT_FLATTEN_HPP
 #define THOT_FLATTEN_HPP
 #include <cstdint>
+#include <utility>
 
+
+#include <torch/nn/pimpl.h>
 #include "../../activation/activation.hpp"
 
 namespace Thot::Layer::Details {
@@ -10,6 +13,39 @@ namespace Thot::Layer::Details {
         std::int64_t start_dim{1};
         std::int64_t end_dim{-1};
     };
+
+    class FlattenImpl : public torch::nn::Module {
+    public:
+        FlattenImpl() = default;
+
+        explicit FlattenImpl(FlattenOptions options)
+        {
+            reset(std::move(options));
+        }
+
+        void reset(FlattenOptions options)
+        {
+            options_ = std::move(options);
+            const auto torch_options = torch::nn::FlattenOptions()
+                                          .start_dim(options_.start_dim)
+                                          .end_dim(options_.end_dim);
+            flatten_ = register_module("flatten", torch::nn::Flatten(torch_options));
+        }
+
+        [[nodiscard]] torch::Tensor forward(torch::Tensor input)
+        {
+            return flatten_->forward(std::move(input));
+        }
+
+        [[nodiscard]] const FlattenOptions& options() const noexcept { return options_; }
+
+    private:
+        FlattenOptions options_{};
+        torch::nn::Flatten flatten_{nullptr};
+    };
+
+    TORCH_MODULE(Flatten);
+
 
     struct FlattenDescriptor {
         FlattenOptions options{};
--- test/main.cpp
+++ test/main.cpp
@@ -6,90 +6,116 @@
 #include "../include/Thot.h"
 
 
-namespace {
-    [[nodiscard]] Thot::Block::Details::ResidualOutputOptions make_residual_output_options()
-    {
-        Thot::Block::Details::ResidualOutputOptions options{};
-        options.final_activation = Thot::Activation::GeLU;
-        options.dropout = 0.1;
-        return options;
-    }
-}
 
 
 int main() {
-    constexpr std::int64_t sequence_length = 6;
-    constexpr std::int64_t embed_dim = 32;
-    constexpr std::int64_t output_dim = 1;
 
 
     Thot::Model model;
     model.to_device(torch::cuda::is_available());
 
-    Thot::Layer::FCOptions input_projection_options{32 * 32 * 3, 32 * 32, true};
-    model.add(Thot::Layer::FC(input_projection_options, Thot::Activation::GeLU));
+    model.add(Thot::Block::Sequential({
+        Thot::Layer::Conv2d(
+            {3, 64, {3, 3}, {1, 1}, {1, 1}, {1, 1}, 1, false},
+            Thot::Activation::Raw,
+            Thot::Initialization::KaimingNormal
+        ),
+        Thot::Layer::BatchNorm2d(
+            {64, 1e-5, 0.1, true, true},
+            Thot::Activation::SiLU
+        ),
+        Thot::Layer::Conv2d(
+            {64, 64, {3, 3}, {1, 1}, {1, 1}, {1, 1}, 1, true},
+            Thot::Activation::Raw,
+            Thot::Initialization::KaimingNormal
+        ),
+        Thot::Layer::BatchNorm2d(
+            {64, 1e-5, 0.1, true, true},
+            Thot::Activation::SiLU
+        ),
+        Thot::Layer::MaxPool2d({{2, 2}, {2, 2}})
+    }));
 
-    Thot::Layer::DropoutOptions input_dropout_options{};
-    input_dropout_options.probability = 0.1;
-    model.add(Thot::Layer::Dropout(input_dropout_options));
-
-    Thot::Block::Transformer::Classic::EncoderOptions encoder_options{};
-    encoder_options.layers = 2;
-    encoder_options.embed_dim = embed_dim;
-    encoder_options.attention.num_heads = 4;
-    encoder_options.attention.dropout = 0.1;
-    encoder_options.feed_forward.mlp_ratio = 2.0;
-    encoder_options.feed_forward.activation = Thot::Activation::GeLU;
-    encoder_options.positional_encoding.type = Thot::Block::Transformer::Classic::PositionalEncodingType::Sinusoidal;
-    encoder_options.positional_encoding.max_length = static_cast<std::size_t>(sequence_length);
-    encoder_options.positional_encoding.dropout = 0.1;
-    encoder_options.dropout = 0.05;
-    model.add(Thot::Block::Transformer::Classic::Encoder(encoder_options));
-
-    Thot::Layer::FlattenOptions flatten_options{};
-    flatten_options.start_dim = 1;
-    flatten_options.end_dim = -1;
-    model.add(Thot::Layer::Flatten(flatten_options));
-
-    Thot::Layer::FCOptions funnel_options{sequence_length * embed_dim, embed_dim, true};
-    model.add(Thot::Layer::FC(funnel_options, Thot::Activation::GeLU));
-
-    std::vector<Thot::Layer::Descriptor> residual_layers{};
-    residual_layers.emplace_back(Thot::Layer::FC({embed_dim, embed_dim, true}, Thot::Activation::GeLU));
-    residual_layers.emplace_back(Thot::Layer::FC({embed_dim, embed_dim, true}, Thot::Activation::Identity));
-    model.add(Thot::Block::Residual(std::move(residual_layers), 2, {}, make_residual_output_options()));
-
-    Thot::Layer::DropoutOptions output_dropout_options{};
-    output_dropout_options.probability = 0.1;
-    model.add(Thot::Layer::Dropout(output_dropout_options));
-
-    model.add(Thot::Layer::FC({embed_dim, output_dim, true}, Thot::Activation::Identity));
-
-    Thot::Optimizer::AdamWOptions optimizer_options{};
-    optimizer_options.learning_rate = 5e-4;
-    optimizer_options.weight_decay = 1e-2;
-    model.set_optimizer(Thot::Optimizer::AdamW(optimizer_options));
-
-    model.set_loss(Thot::Loss::MSE());
-
-    auto [x1, y1, x2, y2] = Thot::Data::Load::CIFAR10("/home/moonfloww/Projects/DATASETS/CIFAR10", 1.f, 1.f, true);
+    model.add(Thot::Layer::Dropout({ .probability = 0.3 }));
 
 
+    model.add(Thot::Block::Residual({
+        Thot::Layer::Conv2d(
+            {64, 64, {3, 3}, {1, 1}, {1, 1}, {1, 1}, 1, false},
+            Thot::Activation::Raw,
+            Thot::Initialization::KaimingNormal
+        ),
+        Thot::Layer::BatchNorm2d(
+            {64, 1e-5, 0.1, true, true},
+            Thot::Activation::SiLU
+        ),
+        Thot::Layer::Conv2d(
+            {64, 64, {3, 3}, {1, 1}, {1, 1}, {1, 1}, 1, true},
+            Thot::Activation::Raw,
+            Thot::Initialization::KaimingNormal
+        )
+    }, 3, {}, { .final_activation = Thot::Activation::SiLU }));
 
-    auto [fx2, fy2] = Thot::Data::Manipulation::Fraction(x2, y2, 0.05f);
+    model.add(Thot::Block::Residual({
+        Thot::Layer::Conv2d(
+            {64, 128, {3, 3}, {2, 2}, {1, 1}, {1, 1}, 1, false},
+            Thot::Activation::Raw,
+            Thot::Initialization::KaimingNormal
+        ),
+        Thot::Layer::BatchNorm2d(
+            {128, 1e-5, 0.1, true, true},
+            Thot::Activation::SiLU
+        ),
+        Thot::Layer::Conv2d(
+            {128, 128, {3, 3}, {1, 1}, {1, 1}, {1, 1}, 1, true},
+            Thot::Activation::Raw,
+            Thot::Initialization::KaimingNormal
+        )
+    }, 1, {}, { .final_activation = Thot::Activation::SiLU }));
 
-    model.train(x1, y1, {.epoch = 120, .batch_size = 32, .test=std::make_pair(fx2, fy2)});
+    model.add(Thot::Layer::Dropout({ .probability = 0.3 }));
 
-    model.eval();
+    model.add(Thot::Block::Sequential({
+        Thot::Layer::Conv2d(
+            {128, 256, {3, 3}, {1, 1}, {1, 1}, {1, 1}, 1, false},
+            Thot::Activation::Raw,
+            Thot::Initialization::KaimingNormal
+        ),
+        Thot::Layer::BatchNorm2d(
+            {256, 1e-5, 0.1, true, true},
+            Thot::Activation::SiLU
+        ),
+        Thot::Layer::AdaptiveAvgPool2d({{1, 1}})
+    }));
 
-    torch::NoGradGuard no_grad;
+    model.add(Thot::Block::Sequential({
+        Thot::Layer::Conv2d(
+            {256, 128, {3, 3}, {1, 1}, {1, 1}, {1, 1}, 1, false},
+            Thot::Activation::Raw,
+            Thot::Initialization::KaimingNormal
+        ),
+        Thot::Layer::BatchNorm2d(
+            {128, 1e-5, 0.1, true, true},
+            Thot::Activation::SiLU
+        ),
+        Thot::Layer::AdaptiveAvgPool2d({{1, 1}})
+    }));
+
+    model.add(Thot::Layer::Flatten());
+
+    model.add(Thot::Layer::FC({128, 512, true}, Thot::Activation::SiLU, Thot::Initialization::KaimingNormal));
+    model.add(Thot::Layer::Dropout({.probability = 0.5}));
+    model.add(Thot::Layer::FC({512, 10, true}, Thot::Activation::Raw, Thot::Initialization::KaimingNormal));
 
 
 
-    auto predictions = model.forward(x2.to(model.device()));
+    model.set_optimizer(Thot::Optimizer::AdamW({.learning_rate = 1e-3, .weight_decay = 5e-4}));
+    model.set_loss(Thot::Loss::CrossEntropy());
 
-    std::cout << "Predictions:\n" << predictions.cpu().squeeze() << '\n';
-    std::cout << "Expected:\n" << y2.squeeze() << '\n';
+    auto [train_images, train_labels, test_images, test_labels] = Thot::Data::Load::CIFAR10("/home/moonfloww/Projects/DATASETS/CIFAR10", 1.f, 1.f, true);
+    auto [validation_images, validation_labels] = Thot::Data::Manipulation::Fraction(test_images, test_labels, 0.1f);
+
+    model.train(train_images, train_labels, {.epoch = 90, .batch_size = 64, .shuffle = true, .test = std::make_pair(validation_images, validation_labels)});
 
     return 0;
 }
