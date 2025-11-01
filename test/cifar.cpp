085ecdd137e8e2cd6d50e7c92fdfe502e02067dd
moonfloww <hugoifiorillo@gmail.com>
2025-11-01 18:08:00 +0100

    timeserie test

--- test/cifar.cpp
+++ test/cifar.cpp
@@ -186,6 +186,7 @@ int main() {
         train_options.restore_best_state = true;
         train_options.enable_amp=true;
         train_options.memory_format = torch::MemoryFormat::ChannelsLast;
+        train_options.epoch_epsilon=true;
         train_options.test = std::make_pair(validation_images, validation_labels);
 
 
