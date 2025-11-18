cc8ad2f491e5ff7e7e88d9d51391b3a4184bf7f2
moonfloww <hugoifiorillo@gmail.com>
2025-11-18 20:16:45 +0100

    up test ntk

--- test/analysis/ntk.cpp
+++ test/analysis/ntk.cpp
@@ -10,7 +10,11 @@ int main() {
     model.add(Thot::Layer::FC({.in_features=16, .out_features=10}, Thot::Activation::Identity, Thot::Initialization::HeUniform));
 
     auto[x1, y1, x2, y2] = Thot::Data::Load::MNIST("/home/moonfloww/Projects/DATASETS/Image/MNIST", 0.1f, 0.1f);
+    auto result = Thot::NTK::Compute(model, x1, y1, {.memory_mode = Thot::NTK::MemoryMode::FullGPU, .print_summary = true});
 
+    std::cout << "Kernel shape: " << result.kernel.sizes() << "\n";
+    std::cout << "Trace: " << result.stats.trace << "\n";
+    std::cout << "Condition number: " << result.stats.condition_number << "\n";
 
     return 0;
 }
